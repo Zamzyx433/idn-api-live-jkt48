0ca00dd5f45acc83707e0bc7bce3d06266241993
Zamzyx433 <anjaskelaanjaskelas@gmail.com>
2025-05-14 09:45:30 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use("/api", routes);
 
-app.get("/", (req, res) => {
+app.get("/jkt48live", (req, res) => {
   res.send({
     message: "Welcome To JKT48 Idn Live API",
   });
